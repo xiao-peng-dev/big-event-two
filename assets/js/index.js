278a95e92498f166b3dd7e5fcfbae2898b2fdf4e
xxxx <you@example.com>
2020-10-29 18:47:59 +0800

    提交首页

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 $(function() {
     getInfo()
+        // 退出
+    $('#btnlogout').on('click', function() {
+        layer.confirm('确定要退出?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
 })
 
 // 获取用户消息
